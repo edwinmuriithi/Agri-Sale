10a9a8db768c31bef880a470757a10b27aad3aec
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-16 16:35:00 +0300

    This commit adds the checkout interface

--- src/main/java/dao/CheckoutDao.java
+++ src/main/java/dao/CheckoutDao.java
@@ -4,5 +4,22 @@ import models.Checkout;
 import models.Feedback;
 import java.util.List;
 
-public class CheckoutDao {
+public interface CheckoutDao {
+    //add a new product to checkout
+    void add(Checkout checkout);
+
+    //view all products in checkout
+    List<Checkout> getAll();
+
+    //view a single product in checkout
+    Checkout findById(int id);
+
+    //update product in checkout
+    void update(int id, String name, String category, int price);
+
+    //delete a single product in checkout
+    void deleteById(int id);
+
+    //delete all products in checkout
+    void clearAll();
 }
