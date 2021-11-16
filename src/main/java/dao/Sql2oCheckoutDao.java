d2d2595ecf173095c6b6985c9546ce688ef03be1
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-16 17:56:30 +0300

    This commit adds the imports for the sql2ocheckout

--- src/main/java/dao/Sql2oCheckoutDao.java
+++ src/main/java/dao/Sql2oCheckoutDao.java
@@ -1,4 +1,11 @@
 package dao;
 
+import models.Products;
+import models.Feedback;
+import models.Checkout;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
 public class Sql2oCheckoutDao {
 }
