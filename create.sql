16bbcb2303af1e99baf5bd1ccbe8be04322cbdfc
ronah289 <ronald.kibet@student.moringaschool.com>
2021-11-16 10:50:22 -0900

    create database schema

---
+++ create.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE agri_sale;
+\c agri_sale
+
+CREATE TABLE products (
+id SERIAL PRIMARY KEY,
+name VARCHAR,
+category VARCHAR,
+price int
+);
+
+CREATE TABLE checkout (
+id SERIAL PRIMARY KEY,
+item_name VARCHAR,
+item_price INT
+);
+
+CREATE TABLE feedback (
+id SERIAL PRIMARY KEY,
+name VARCHAR,
+email VARCHAR,
+message VARCHAR
+);
+
+CREATE DATABASE agri_sale_test WITH TEMPLATE agri_sale;
