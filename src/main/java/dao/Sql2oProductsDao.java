31d5ad8246104edc7b1dfc76a856eabe99917ce4
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-16 13:28:12 +0300

    This commit adds the add() method in the Sql2oProduct to add a new product

--- src/main/java/dao/Sql2oProductsDao.java
+++ src/main/java/dao/Sql2oProductsDao.java
@@ -8,6 +8,26 @@ import org.sql2o.Sql2oException;
 
 import java.util.ArrayList;
 import java.util.List;
-public class Sql2oProductsDao {
+public class Sql2oProductsDao implements ProductsDao {
+
+    private final Sql2o sql2o;
+    public Sql2oProductsDao (Sql2o sql2o)
+    {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Products products) {
+        String sql = "INSERT INTO products (name, category, price) VALUES (:name, :category, :price)";
+        try(Connection con = sql2o.open()){
+            int id = (int)con.createQuery(sql, true)
+                    .bind(products)
+                    .executeUpdate()
+                    .getKey();
+            products.setId(id);
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
+    }
 
 }
