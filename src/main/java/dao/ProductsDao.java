8dd41bc1ffadb1d23f78d33ae90e75366d16c8a5
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-16 13:19:40 +0300

    This commit adds the add, view all, view one and delete to the ProductsDao interface

--- src/main/java/dao/ProductsDao.java
+++ src/main/java/dao/ProductsDao.java
@@ -4,3 +4,21 @@ import models.Checkout;
 import models.Feedback;
 import java.util.List;
 
+public interface ProductsDao{
+    //add a new product
+    void add(Products products);
+
+    //view all products
+    List<Products> getAll();
+
+    //view a single product
+    Products findById(int id);
+
+    //update
+    void update(int id, String name, String category, int price);
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+}
+
