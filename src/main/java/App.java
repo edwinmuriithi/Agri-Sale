142f8ffadabc08079e9a38cf18845c1f2936e722
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-06-17 16:38:56 +0300

    Edit Java Class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -23,7 +23,7 @@ public class App {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4568; //return default port if heroku-port isn't set (i.e. on localhost)
+//        return 4568; //return default port if heroku-port isn't set (i.e. on localhost)/
     }
 
     public static void main(String[] args) {
